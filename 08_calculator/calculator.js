4e870bab60da86c8bcd3ef51dae0ffccef3d2080
cunzo <58085535+cunzo@users.noreply.github.com>
2024-01-27 18:22:52 +0100

    COMPLETE!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let tot = 0;
+  a.forEach(element => {
+    tot += element;
+  });
+  return tot;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((accumulator, current) => accumulator * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for (let i = 2; i <= a; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
