aec1d4e23edb0d009d2ae3d1ddeed9df0f6687d3
cunzo <58085535+cunzo@users.noreply.github.com>
2024-01-28 18:07:54 +0100

    COMPLETE!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1) return 1;
 
+    let a = 0;
+    let b = 1;
+    let temp;
+
+    for (let i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
