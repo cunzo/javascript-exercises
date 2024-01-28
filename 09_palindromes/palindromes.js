f65708b01c1162b027391f3e197605bae35a4c7d
cunzo <58085535+cunzo@users.noreply.github.com>
2024-01-28 17:47:33 +0100

    COMPLETE!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    if (typeof a !== "string") {return 'non e` possibile stabilire se la parola/frase inserita e` palindroma';}
+    let arr = a.split("");
+    arr = arr.filter(element => /^[A-Za-z0-9]$/.test(element));
+    arr2 = arr.slice().reverse();
+    if (arr.join('').toLowerCase() === arr2.join('').toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
