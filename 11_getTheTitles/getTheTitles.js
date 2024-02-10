4629e0d24db0b4d0c32c2c0ce0e668b255f501bb
cunzo <58085535+cunzo@users.noreply.github.com>
2024-02-11 00:28:45 +0100

    COMPLETE !

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
+// const getTheTitles = function(books) {
+//     let a = [];
+//     books.forEach(element => {
+//         a.push(element.title);
+//     });
+//     return a;
+// };
 
+const getTheTitles = function(books) {
+    return books.map((book) => book.title);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
