2a72ca1196f000678710bca04a0203fdc0743e7d
cunzo <58085535+cunzo@users.noreply.github.com>
2024-02-11 02:16:55 +0100

    COMPLETE!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const thisYear = new Date().getFullYear();
+    people.forEach(element => {
+        if (element.yearOfDeath != undefined) {
+            element.age = element.yearOfDeath - element.yearOfBirth;
+        } else {
+            element.age = thisYear - element.yearOfBirth;
+        }
+    });
 
+    let oldestPerson = null;
+
+    people.forEach(element => {
+        if (oldestPerson === null || element.age > oldestPerson.age) {
+            oldestPerson = element;
+        }
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
